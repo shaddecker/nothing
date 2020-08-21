03c45f47943f8ba3046b02b0c6651abe1e73afca
shaddecker <shaddecker5@gmail.com>
2020-08-21 12:37:14 -0500

    converted app.js to class

---
+++ nothing/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Component.css';
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+          
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
