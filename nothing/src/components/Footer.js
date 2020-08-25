e8fe4d65a3f355e007ae090bdaf1f8e29e0f251b
shaddecker <shaddecker5@gmail.com>
2020-08-25 13:53:15 -0500

    tweaking footer

--- nothing/src/components/Footer.js
+++ nothing/src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import logo from "../logo.svg";
 import './Component.css';
 
 const Footer = () => {
+  let lastModified = document.lastModified;
   return (
     
       <footer className="footerContainer">
-        <div>Site created by team Yada Yada Yada</div>
+        <div className="footerTitle">
+          Site created by team Yada Yada Yada <br/>
+          Using <a href="https://reactjs.org/" target="_blank">React</a> and <a href="https://www.npmjs.com/package/axios" target="_blank">Axios</a><br/>
+          Last Modified: {lastModified}
+        </div>
+        <div className="footerDeadSpace"></div>
+        <div className="footerLogo"><img src={logo} className="App-logo" alt="logo" /></div>
       </footer>
   )
 }
