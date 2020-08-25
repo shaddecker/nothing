f997d54d1c6f748748a68948d449885c3c9f8654
shaddecker <shaddecker5@gmail.com>
2020-08-25 13:17:09 -0500

    did some css clean up

--- nothing/src/components/Footer.js
+++ nothing/src/components/Footer.js
@@ -3,11 +3,10 @@ import './Component.css';
 
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <footer>
+    
+      <footer className="footerContainer">
         <div>Site created by team Yada Yada Yada</div>
       </footer>
-    </div>
   )
 }
 
