f830017de144b1c5c3663733854f8d46f05cd0d9
shaddecker <shaddecker5@gmail.com>
2020-08-26 10:43:45 -0500

    map popup working

--- nothing/src/components/MapPopup.js
+++ nothing/src/components/MapPopup.js
@@ -1,22 +1,54 @@
-import React, {Component} from 'react'
-  
-class MapPopup extends Component{
-    constructor(props){
-        super(props)
-    }
-    handleClick = () => {
-       this.props.mapVisible();
-    };
+import React, {Component} from 'react';
+import Modal from "react-modal";
+import style from "./Component.css";  
 
-    render() {
-      return (
-        <div className="modalWindow">
-            <div className="modalContent" onClick={this.handleClick}>
-            <img src={this.props.map} alt="map"/>
-            </div>
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+}
+
+Modal.setAppElement(document.getElementById('root'));
+
+class MapPopup extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      windowOpened: false,
+    }
+  }
+    
+  toggleShowMap = () => {
+    this.setState({windowOpened: !this.state.windowOpened});
+  }
+
+  
+  render() {
+    console.log(this.props.mapData, this.state.windowOpened)
+  return (
+        <div>
+        <button className="button" onClick={this.toggleShowMap}>Show Map </button><br />
+        <Modal 
+          isOpen={this.state.windowOpened}
+          onRequestClose={this.toggleShowMap}
+          style={customStyles}
+          contentLabel="Modal with image"
+        >
+          <div className="contentTitle">Your Location (or pretty close to it)</div>
+          <img
+            onClick={this.toggleShowMap}
+            src={this.props.mapData}
+            alt=''
+          />          
+        </Modal>        
         </div>
       );
-     }
-    }
+  }
+}
 
-  export default MapPopup
+  export default MapPopup;
